1ffe674a397782c809d2cede631bd4c9b203f6ed
Andrew Prosikhin <andrewpros@mail.ru>
2017-03-19 19:36:45 -0400

    Scenario III: 50% buffer utilization.

--- src/controller/SystemTest.java
+++ src/controller/SystemTest.java
@@ -17,6 +17,11 @@ public class SystemTest {
 			testPasses = false;
 		}
 
+		boolean scenario3 = testScenario3();
+		if (!scenario3) {
+			testPasses = false;
+		}
+
 		if (testPasses) {
 			System.out.println("Full System Test: PASS");
 			System.exit(0);
@@ -110,6 +115,51 @@ public class SystemTest {
 		double averageQueueLength = analyzer.averageBufferQueueLength();
 		if (averageQueueLength > 0.00001) {
 			System.out.println("	Expected average buffer queue length to be 0.0. Got: " + averageQueueLength + ".");
+			return false;
+		}
+
+		return true;
+	}
+
+	//SCENARIO 3: 50% buffer utilization.
+	private static boolean testScenario3() {
+		Controller contr = new Controller(1000000, 50000, 10);
+		contr.run();
+		Analyzer analyzer = contr.analyzer();
+
+		int numReceived = analyzer.numberOfReceivedPackets();
+		if (numReceived > 50000 || numReceived < 49998) {
+			System.out.println("	Expected 49998-50000 received packets. Got: " + numReceived + ".");
+			return false;
+		}
+
+		int minTravelTime = analyzer.minimumTravelTime();
+		if (minTravelTime != 10) {
+			System.out.println("	Expected minimum travel time for packets to be 10. Got: " + minTravelTime + ".");
+			return false;
+		}
+
+		int maxTravelTime = analyzer.maximumTravelTime();
+		if (maxTravelTime < 65 || maxTravelTime > 125) {
+			System.out.println("	Expected maximim travel time for packets to be in range 65-110. Got: " + maxTravelTime + ".");
+			return false;
+		}
+
+		double averageTravelTime = analyzer.averageTravelTime();
+		if (averageTravelTime < 14.35 || averageTravelTime > 14.65) {
+			System.out.println("	Expected average travel time for packets to be in range 14.4 to 14.55. Got: " + averageTravelTime + ".");
+			return false;
+		}
+
+		int maxBufferQueue = analyzer.maximumBufferQueueLength();
+		if (maxBufferQueue < 6 || maxBufferQueue > 12) {
+			System.out.println("	Expected maximim buffer queue length to be in range 6-12. Got: " + maxBufferQueue + ".");
+			return false;
+		}
+
+		double averageQueueLength = analyzer.averageBufferQueueLength();
+		if (averageQueueLength < 0.65 || averageQueueLength > 0.70) {
+			System.out.println("	Expected average buffer queue length to be in range 0.65 to 0.68. Got: " + averageQueueLength + ".");
 			return false;
 		}
 
